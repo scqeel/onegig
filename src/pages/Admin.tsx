386904030605de24fd9a92eb63d94503b97fbabf
hp <sunumanfred14@gmail.com>
2026-08-02 01:37:04 +0100

    Fix Link is not defined ReferenceError in Admin.tsx

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import {
   Activity, BellRing, CheckCircle2, Clock, Cog, DollarSign, Globe2,
